aa8664c7be03533319826a582577e311d08ef4dc
Liaquat Ali <liaquatali.centosquare@gmail.com>
2023-01-30 13:57:20 +0500

    add products and add shopping and profile icons

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,6 +20,28 @@ class ProductSeeder extends Seeder
             'description' => 'Good watch',
             'image' => 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=989&q=80'
         ]);
+
+        Product::create([
+            'name' => 'Watch',
+            'price' => 280,
+            'description' => 'Good watch',
+            'image' => 'https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'Watch',
+            'price' => 300,
+            'description' => 'Good watch',
+            'image' => 'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'Watch',
+            'price' => 250,
+            'description' => 'Good watch',
+            'image' => 'https://images.unsplash.com/photo-1539874754764-5a96559165b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'
+        ]);
+
         Product::create([
             'name' => 'Bag',
             'price' => 350,
@@ -35,17 +57,31 @@ class ProductSeeder extends Seeder
         ]);
 
         Product::create([
-            'name' => 'perfume',
-            'price' => 100,
-            'description' => 'Good perfume',
-            'image' => 'https://images.unsplash.com/photo-1528740561666-dc2479dc08ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80'
+            'name' => 'Bag',
+            'price' => 300,
+            'description' => 'Good Bag',
+            'image' => 'https://images.unsplash.com/photo-1525103504173-8dc1582c7430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'Bag',
+            'price' => 290,
+            'description' => 'Good Bag',
+            'image' => 'https://images.unsplash.com/photo-1568247067952-eab2ce84a349?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
         ]);
 
         Product::create([
             'name' => 'laptop',
             'price' => 500,
             'description' => 'Good laptop',
-            'image' => 'https://images.unsplash.com/photo-1555960840-f536ae1e4e95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGxhcCUyMHNjcmVlbnxlbnwwfDF8MHx8&auto=format&fit=crop&w=500&q=60'
+            'image' => 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'laptop',
+            'price' => 500,
+            'description' => 'Good laptop',
+            'image' => 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80'
         ]);
 
         Product::create([
@@ -59,7 +95,7 @@ class ProductSeeder extends Seeder
             'name' => 'laptop',
             'price' => 50,
             'description' => 'Good laptop',
-            'image' => 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80'
+            'image' => 'https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
         ]);
 
         Product::create([
@@ -74,6 +110,49 @@ class ProductSeeder extends Seeder
             'price' => 300,
             'description' => 'Good mobile',
             'image' => 'https://images.unsplash.com/photo-1567581935884-3349723552ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bW9iaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        ]);
+
+        Product::create([
+            'name' => 'mobile',
+            'price' => 310,
+            'description' => 'Good mobile',
+            'image' => 'https://images.unsplash.com/photo-1546027658-7aa750153465?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'mobile',
+            'price' => 200,
+            'description' => 'Good mobile',
+            'image' => 'https://images.unsplash.com/photo-1596558450268-9c27524ba856?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80'
+        ]);
+
+        
+        Product::create([
+            'name' => 'perfume',
+            'price' => 100,
+            'description' => 'Good perfume',
+            'image' => 'https://images.unsplash.com/photo-1528740561666-dc2479dc08ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'perfume',
+            'price' => 120,
+            'description' => 'Good perfume',
+            'image' => 'https://images.unsplash.com/photo-1623742310401-d8057c3c43c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'perfume',
+            'price' => 150,
+            'description' => 'Good perfume',
+            'image' => 'https://images.unsplash.com/photo-1624613533305-28d421d70875?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+        ]);
+
+        Product::create([
+            'name' => 'perfume',
+            'price' => 120,
+            'description' => 'Good perfume',
+            'image' => 'https://images.unsplash.com/photo-1589782431327-ac8a63d965a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
         ]);
     }
 }
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -16,7 +16,7 @@
                     
                     
                     <div class="flex items-center justify-end w-full">
-                        <button" class="mx-4 text-gray-600 focus:outline-none sm:mx-0">
+                        <button class="mx-4 text-gray-600 focus:outline-none sm:mx-0">
                             
                         </button>
                     </div>
@@ -24,10 +24,15 @@
                 <nav  class="p-6 mt-4 text-white bg-black sm:flex sm:justify-center sm:items-center">
                     <div class="flex flex-col sm:flex-row">
                         <a class="mt-3 hover:underline sm:mx-3 sm:mt-0" href="/">Shop</a>
-                        <a href="{{ route('cart.list') }}" class="flex items-center">
-                          profile  
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus-fill mr-12 mt-1" viewBox="0 0 16 16">
+                            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                          </svg>
+                        <a href="{{ route('cart.list') }}" class="flex items-center mr-2">
+                          Profile  
                         </a>
-                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill mt-1 " viewBox="0 0 16 16">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                          </svg>
                     </div>
                 </nav>
             </div>
